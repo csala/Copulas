15d2fa5a26938b054cc5e201e62e49a434cb855f
Carles Sala <carles@pythiac.com>
2020-05-18 17:36:33 +0200

    Add python 3.8

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="A python library for building different types of copulas and using them for sampling.",
     entry_points={
